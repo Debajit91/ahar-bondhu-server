aff1acd3fe2f97b6aaea40266481d7fd54a55236
Debajit91 <debajitroy544@gmail.com>
2025-07-22 04:57:34 +0600

    project setup with express dotenv cors and mongodb

---
+++ DB/connect.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require("mongodb");
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khtcm39.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri);
+
+const connectToDB = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client.db("yourDBName"); // change as needed
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
+
+module.exports = connectToDB;
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectToDB = require("./DB/connect");
+
+
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+connectToDB().then((db) => {
+  
+
+  app.get("/", (req, res) => {
+    res.send("Ahar Bondhu Server is running...");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
