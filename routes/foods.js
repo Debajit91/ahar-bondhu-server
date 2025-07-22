ed5f1149cfe1b1e888f9502d16441b35428986c9
Debajit91 <debajitroy544@gmail.com>
2025-07-23 01:33:19 +0600

    route for single details page implemented

--- routes/foods.js
+++ routes/foods.js
@@ -1,21 +1,40 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 
 module.exports = (db) => {
   const foodCollection = db.collection("foods");
 
   router.get("/", async (req, res) => {
-  const sortBy = req.query.sortBy;
-  const filter = { status: "available" };
+    const sortBy = req.query.sortBy;
+    const filter = { status: "available" };
 
-  const foods = await db.collection("foods")
-    .find(filter)
-    .sort(sortBy === "expireDate" ? { expireDate: 1 } : {})
-    .toArray();
+    const foods = await db
+      .collection("foods")
+      .find(filter)
+      .sort(sortBy === "expireDate" ? { expireDate: 1 } : {})
+      .toArray();
 
-  res.send(foods);
-});
+    res.send(foods);
+  });
 
+  //   single food details route
+  router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const food = await db
+        .collection("foods")
+        .findOne({ _id: new ObjectId(id) });
+
+      if (!food) {
+        return res.status(404).send({ message: "Food not found" });
+      }
+
+      res.send(food);
+    } catch (error) {
+      res.status(500).send({ message: "Server error" });
+    }
+  });
 
   // POST: Add Food
   router.post("/", async (req, res) => {
@@ -25,6 +44,28 @@ module.exports = (db) => {
     } catch (err) {
       console.error("Error adding food:", err);
       res.status(500).send({ error: "Failed to add food" });
+    }
+  });
+
+  router.patch("/request/:id", async (req, res) => {
+    const { id } = req.params;
+    const { notes, userEmail, requestDate } = req.body;
+
+    try {
+      await db.collection("foods").updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $set: {
+            status: "requested",
+            requestedBy: userEmail,
+            requestDate,
+            notes,
+          },
+        }
+      );
+      res.send({ message: "Request successful" });
+    } catch (err) {
+      res.status(500).send({ error: "Update failed" });
     }
   });
 
